54e98944b7eae7d2bca2f2ab4361f9ae2d19004b
Marcos Navidad <145333083+MarcosNavLeo@users.noreply.github.com>
2024-05-01 20:41:22 +0200

    Creacion/Borrar_Mascotas

--- public/js/home.js
+++ public/js/home.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
   var btnañadirMasco = $('#btnañadirMasco');
   $('#fechaNacimientoMascota').datepicker({
     dateFormat: 'dd-mm-yy',
+    maxDate: 0,
     changeYear: true
   });
+
   btnañadirMasco.click(function () {
     $('#addPetModal').modal('show');
   });
@@ -11,10 +13,12 @@ $(document).ready(function () {
   $('#addPetModal .btn-secondary, #addPetModal .close').click(function () {
     $('#addPetModal').modal('hide');
   });
+  $('#cargando').show();
   $.ajax({
     url: '/api/mascotas/' + idUser,
     type: 'GET',
     success: function (data) {
+      $('#cargando').hide();
       var row = $('<div />', { class: 'row' });
       $.each(data, function (index, mascota) {
         var img = $('<img />', {
@@ -23,6 +27,7 @@ $(document).ready(function () {
           class: 'card-img-top custom-img-size'
         });
         var nombre = $('<h5 />', { class: 'card-title' }).text(mascota.nombre);
+        var fechaNacimiento = $('<span />', { class: 'card-subtitle mb-2 text-muted' }).text(mascota.fechaNacimiento);
         var raza = $('<p />', { class: 'card-text' }).html('<strong>Raza:</strong> ' + mascota.raza);
         var especie = $('<p />', { class: 'card-text' }).html('<strong>Especie:</strong> ' + mascota.especie);
 
@@ -30,9 +35,56 @@ $(document).ready(function () {
         var especieColumn = $('<div />', { class: 'col-md-6' }).append(especie);
         var infoRow = $('<div />', { class: 'row' }).append(razaColumn, especieColumn);
 
-        var cardBody = $('<div />', { class: 'card-body' }).append(nombre, infoRow);
+        // Crear el botón de eliminar
+        var deleteButton = $('<button />', {
+          text: 'Eliminar',
+          class: 'btn btn-danger',
+          click: function () {
+            // Aquí puedes añadir la lógica para eliminar la mascota
+            console.log('Eliminar mascota con id ' + mascota.id);
+            Swal.fire({
+              title: '¿Estás seguro?',
+              text: '¿Deseas eliminar esta mascota?',
+              icon: 'question',
+              showCancelButton: true,
+              confirmButtonColor: '#d33',
+              cancelButtonColor: '#3085d6',
+              confirmButtonText: 'Sí, eliminar',
+              cancelButtonText: 'Cancelar'
+            }).then((result) => {
+              if (result.isConfirmed) {
+                $.ajax({
+                  url: '/api/mascotas/delete/' + mascota.id,
+                  type: 'DELETE',
+                  success: function () {
+                    // Eliminar la mascota del DOM
+                    cardCol.remove();
+                    // Mostrar mensaje de éxito con SweetAlert
+                    Swal.fire({
+                      icon: 'success',
+                      title: 'Mascota eliminada correctamente',
+                      showConfirmButton: false,
+                      timer: 1500
+                    });
+                  },
+                  error: function () {
+                    // Mostrar mensaje de error con SweetAlert
+                    Swal.fire({
+                      icon: 'error',
+                      title: 'Error al eliminar la mascota',
+                      text: 'Por favor, inténtalo de nuevo más tarde.',
+                      showConfirmButton: true
+                    });
+                  }
+                });
+              }
+            });
+          }
+        });
+
+        var cardBody = $('<div />', { class: 'card-body' }).append(nombre, fechaNacimiento, infoRow, deleteButton);
         var card = $('<div />', {
-          class: 'card'
+          class: 'card mb-4'  // Añadir un margen inferior
         }).append(img, cardBody);
         var cardCol = $('<div />', {
           class: 'col-md-4'
@@ -46,7 +98,91 @@ $(document).ready(function () {
     }
   });
 
-  
+  // Manejador de eventos para el botón "Guardar cambios"
+  $('#guardarCambios').click(function () {
+    // Obtener los datos del formulario
+    var nombre = $('#nombreMascota').val();
+    var especie = $('#especieMascota').val();
+    var raza = $('#razaMascota').val();
+    var fechaNacimiento = $('#fechaNacimientoMascota').val();
+    var foto = $('#fotoMascota')[0].files[0];
+
+    // Mostrar confirmación al usuario
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: '¿Deseas añadir esta mascota?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, añadir',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Crear un objeto FormData
+        var formData = new FormData();
+        formData.append('nombre', nombre);
+        formData.append('especie', especie);
+        formData.append('raza', raza);
+        formData.append('fechaNacimiento', fechaNacimiento);
+        formData.append('foto', foto);
+        formData.append('propietario', idUser);
+
+        // Enviar los datos al servidor mediante AJAX
+        $.ajax({
+          url: '/api/mascotas/create/' + idUser,
+          type: 'POST',
+          data: formData,
+          contentType: false,
+          processData: false,
+          success: function (mascota) {
+            // Agregar la nueva mascota al contenedor
+            var img = $('<img />', {
+              src: mascota.foto,
+              alt: mascota.nombre,
+              class: 'card-img-top custom-img-size'
+            });
+            var nombreElement = $('<h5 />', { class: 'card-title' }).text(mascota.nombre);
+            var razaElement = $('<p />', { class: 'card-text' }).html('<strong>Raza:</strong> ' + mascota.raza);
+            var especieElement = $('<p />', { class: 'card-text' }).html('<strong>Especie:</strong> ' + mascota.especie);
+            var razaColumn = $('<div />', { class: 'col-md-6' }).append(razaElement);
+            var especieColumn = $('<div />', { class: 'col-md-6' }).append(especieElement);
+            var infoRow = $('<div />', { class: 'row' }).append(razaColumn, especieColumn);
+            var cardBody = $('<div />', { class: 'card-body' }).append(nombreElement, infoRow);
+            var card = $('<div />', {
+              class: 'card'
+            }).append(img, cardBody);
+            var cardCol = $('<div />', {
+              class: 'col-md-4'
+            }).append(card);
+            $('#mascotas-container').prepend(cardCol); // Agregar la mascota al principio del contenedor
+            // Mostrar mensaje de éxito con SweetAlert
+            Swal.fire({
+              icon: 'success',
+              title: 'Mascota creada correctamente',
+              showConfirmButton: false,
+              timer: 1500
+            });
+            // Cerrar el modal
+            $('#addPetModal').modal('hide');
+          },
+          error: function (error) {
+            // Mostrar mensaje de error con SweetAlert
+            Swal.fire({
+              icon: 'error',
+              title: 'Error al crear la mascota',
+              text: 'Por favor, inténtalo de nuevo más tarde.',
+              showConfirmButton: true
+            });
+            console.error('Error al crear la mascota:', error);
+          }
+        });
+      }
+    });
+  });
+
+
 });
+
 
 
--- src/Controller/api/apiMascotas.php
+++ src/Controller/api/apiMascotas.php
@@ -3,7 +3,6 @@
 namespace App\Controller\api;
 
 use App\Repository\MascotasRepository;
-use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +14,7 @@ use App\Repository\UserRepository;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use App\Entity\Mascotas;
 use Doctrine\ORM\EntityManagerInterface;
-
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class apiMascotas extends AbstractController
 {
@@ -53,18 +52,6 @@ class apiMascotas extends AbstractController
         return $this->createResponse($mascotas);
     }
 
-    #[Route('/api/mascotas/{nombre}/{parametro}', name: 'api_mascota_categoria')]
-    public function getMascotasByCategoria(Request $request, MascotasRepository $mascotaRepository, $nombre, $parametro): JsonResponse
-    {
-        $mascotas = $mascotaRepository->findByLocalidadOProvincia($parametro, $nombre);
-
-        if (!$mascotas) {
-            throw new NotFoundHttpException('Mascotas no encontradas');
-        }
-
-        return $this->createResponse($mascotas);
-    }
-
     private function createResponse($mascotas): JsonResponse
     {
         $mascotasArray = [];
@@ -74,43 +61,80 @@ class apiMascotas extends AbstractController
                 'nombre' => $mascota->getNombre(),
                 'especie' => $mascota->getEspecie(),
                 'raza' => $mascota->getRaza(),
+                'fechaNacimiento' => $mascota->getFechaNacimiento()->format('Y-m-d'), 
                 'foto' => $mascota->getFoto(),
             ];
         }
 
         return new JsonResponse($mascotasArray);
     }
-    #[Route('/api/mascotas/{idUser}', name: 'api_mascotas_create', methods: ['POST'])]
-    public function createMascota(Request $request, MascotasRepository $mascotaRepository, EntityManagerInterface $entityManager,$idUser): JsonResponse
+
+    #[Route('/api/mascotas/create/{idUser}', name: 'api_mascotas_create', methods: ['POST'])]
+    public function createMascota(Request $request, MascotasRepository $mascotaRepository, $idUser, UserRepository $userRepository, EntityManagerInterface $entityManagerInterface): JsonResponse
     {
-        $data = json_decode($request->getContent(), true);
+        $nombre = $request->get('nombre');
+        $especie = $request->get('especie');
+        $raza = $request->get('raza');
+        $fechaNacimiento = $request->get('fechaNacimiento');
 
-        if ($data === null) {
-            throw new BadRequestHttpException('Invalid JSON');
+
+        // Obtener la foto de la solicitud HTTP
+        $fotoFile = $request->files->get('foto');
+
+        // Procesar y guardar la foto
+        if ($fotoFile) {
+            // Generar un nombre único y aleatorio para la imagen
+            $fotoFileName = md5(uniqid()) . '.' . $fotoFile->guessExtension();
+
+            // Mover la imagen al directorio de destino
+            try {
+                $fotoFile->move($this->getParameter('imagenes_directory'), $fotoFileName);
+            } catch (FileException $e) {
+                throw new BadRequestHttpException('No se pudo mover la imagen');
+            }
+            // Incluir el directorio de las imágenes en el nombre del archivo
+            $fotoFileName = 'imagenes/mascotas/' . $fotoFileName;
+        } else {
+            // Si no se proporciona una imagen, establecer el nombre de la foto como null en la base de datos
+            $fotoFileName = null;
         }
 
-        if (!isset($data['nombre']) || !isset($data['especie']) || !isset($data['raza']) || !isset($data['foto']) || !isset($data['fechaNacimiento']) || !isset($data['user'])) {
-            return new JsonResponse(['status' => 'Error', 'message' => 'Faltan datos necesarios'], JsonResponse::HTTP_BAD_REQUEST);
-        }
-        $nombre = $data['nombre'];
-        $especie = $data['especie'];
-        $raza = $data['raza'];
-        $foto = $data['foto'];
-        $fechaNacimiento = $data['fechaNacimiento'];
+        // Obtener el objeto User correspondiente al ID del usuario
+        $user = $userRepository->find($idUser);
 
+        if (!$user) {
+            return new JsonResponse(['status' => 'Error', 'message' => 'Usuario no encontrado'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        // Crear una nueva instancia de Mascotas y establecer los datos
         $mascota = new Mascotas();
         $mascota->setNombre($nombre);
         $mascota->setEspecie($especie);
         $mascota->setRaza($raza);
-        $mascota->setFoto($foto);
-        $mascota->setFechaNacimiento($fechaNacimiento);
-        $mascota->setUser($idUser);
+        $mascota->setFechaNacimiento(new \DateTime($fechaNacimiento));
+        $mascota->setFoto($fotoFileName);
+        $mascota->setUser($user);  
 
-        $entityManager->persist($mascota);
-        $entityManager->flush();
 
-        return new JsonResponse(['status' => 'Mascota creada'], JsonResponse::HTTP_CREATED);
+        // Persistir la entidad
+        $entityManagerInterface->persist($mascota);
+        $entityManagerInterface->flush();
+        // Return a JSON response with the error message
+        return new JsonResponse(['status' => 'Error', 'message' => 'Error al crear la mascota: '], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    #[Route('/api/mascotas/delete/{id}', name: 'api_mascotas_delete', methods: ['DELETE'])]
+    public function deleteMascota(Request $request, MascotasRepository $mascotaRepository, $id, EntityManagerInterface $entityManagerInterface): JsonResponse
+    {
+        $mascota = $mascotaRepository->find($id);
+
+        if (!$mascota) {
+            return new JsonResponse(['status' => 'Error', 'message' => 'Mascota no encontrada'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        $entityManagerInterface->remove($mascota);
+        $entityManagerInterface->flush();
+
+        return new JsonResponse(['status' => 'OK', 'message' => 'Mascota eliminada'], JsonResponse::HTTP_OK);
     }
 }
-
-
